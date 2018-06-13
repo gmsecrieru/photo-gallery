a43a3096c0717ac19854a9bbfc5d6b8e3bab11b0
George Secrieru <george.secrieru@gmail.com>
2018-06-13 18:37:25 -0300

    Add Spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './index.css'
+
+/**
+ * Disclaimer: This spinner was copied from http://tobiasahlin.com/spinkit/
+ */
+export default function Spinner() {
+  return <div className="spinner" />
+}
