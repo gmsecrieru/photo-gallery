024d09f4458dc1ec8c6d496ee61b6105f60a7a25
George Secrieru <george.secrieru@gmail.com>
2018-06-14 14:27:55 -0300

    Use Lightbox component for displaying PhotoDetails when a Photo is clicked

--- src/App.js
+++ src/App.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react';
 
 import './App.css'
 
+import Lightbox from './components/Lightbox'
+import PhotoDetails from './components/PhotoDetails'
 import PhotoStreamContainer from './containers/PhotoStreamContainer'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handlePhotoClick = this.handlePhotoClick.bind(this)
+    this.handleLightboxClick = this.handleLightboxClick.bind(this)
+
+    this.state = {
+      isLightBoxOpen: false,
+      photoDetailsProps: null
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Photo Gallery</h1>
-        <PhotoStreamContainer />
+        {
+          this.state.isLightBoxOpen
+          ? <Lightbox onClick={this.handleLightboxClick}>
+              <PhotoDetails {...this.state.photoDetailsProps} />
+            </Lightbox>
+          : null
+        }
+        <PhotoStreamContainer onClick={this.handlePhotoClick}/>
       </div>
     );
+  }
+
+  handlePhotoClick(photo) {
+    this.setState({
+      isLightBoxOpen: true,
+      photoDetailsProps: photo.props
+    })
+  }
+
+  handleLightboxClick() {
+    this.setState({
+      isLightBoxOpen: false
+    })
   }
 }
 
